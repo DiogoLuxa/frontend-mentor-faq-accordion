1e73a63f76900de47d3a0e0e37c23d92db094684
DiogoLuxa <diogoluxa.dev@gmail.com>
2024-04-10 01:01:13 -0300

    atualizacao acessibilidade focus e teclado

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,50 @@
+// VARIABLES
 const accordionContentBlock = document.querySelectorAll(
   ".display-content__block"
 );
+const accordionBlockTitle = document.querySelectorAll(
+  ".display-content__block__title"
+);
+const accordionBlockContent = document.querySelectorAll(".content");
+const accordionBlockIcon = document.querySelectorAll(".btn-icon");
 
-accordionContentBlock.forEach((content) => {
-  const blockTitle = content.querySelectorAll(".display-content__block__title");
-  const blockBody = content.querySelectorAll(".body");
-  const blockIcon = content.querySelectorAll(".btn-icon");
+// FUNCTIONS
+function addActiveClass(index) {
+  accordionBlockContent[index].classList.add("active");
+}
+function removeActiveClasses() {
+  accordionBlockContent.forEach((content) => {
+    content.classList.remove("active");
+  });
+}
+function changeIconMinus(i) {
+  accordionBlockIcon[i].src = "./assets/images/icon-minus.svg";
+}
+function changeIconPlus() {
+  accordionBlockIcon.forEach((icon) => {
+    icon.src = "./assets/images/icon-Plus.svg";
+  });
+}
 
-  blockTitle.forEach((title, i) => {
-    title.addEventListener("click", () => {
-      accordionContentBlock.forEach((content) => {
-        content.querySelector(".body").classList.remove("active");
-        content.querySelector(".btn-icon").src =
-          "./assets/images/icon-plus.svg";
-      });
-      blockIcon[i].src = "./assets/images/icon-minus.svg";
-      blockBody[i].classList.add("active");
-    });
+// EVENT LISTENERS
+accordionBlockTitle.forEach((title, i) => {
+  title.addEventListener("click", () => {
+    let expanded = title.getAttribute("aria-expanded") === "true" || false;
+    title.setAttribute("aria-expanded", !expanded);
+    removeActiveClasses();
+    changeIconPlus();
+    addActiveClass(i);
+    changeIconMinus(i);
+  });
+
+  title.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      let expanded = title.getAttribute("aria-expanded") === "true" || false;
+      title.setAttribute("aria-expanded", !expanded);
+      removeActiveClasses();
+      changeIconPlus();
+      addActiveClass(i);
+      changeIconMinus(i);
+    }
   });
 });
